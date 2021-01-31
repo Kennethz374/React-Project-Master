1272108e3b6b630fcdebd81fa60646967272d394
Kenneth <KennethZhang374@gmail.com>
2021-01-31 00:01:12 -0800

    done

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -38,7 +38,19 @@ export default function App() {
 
       <div className="jobs-center">
         {/* btn container */}
-        {/* job info */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && "active-btn"}`}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
         <article className="job-info">
           <h3>{title}</h3>
           <h4>{company}</h4>
