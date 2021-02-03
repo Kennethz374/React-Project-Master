0f455b90182e108173e876a5177d32415913098b
Kenneth <KennethZhang374@gmail.com>
2021-02-03 11:40:57 -0800

    able to access hex now

--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
-import rgbToHex from "./utils";
 
-const SingleColor = ({ rgb, weight, index }) => {
+const SingleColor = ({ rgb, index, weight, hexColor }) => {
   const [alter, setAlert] = useState(false);
   const bcg = rgb.join(","); //turn array to string
 
   return (
     <article className={`color`} style={{ backgroundColor: `rgb(${bcg})` }}>
       <p className="percent-value">{weight}%</p>
+      <p className="color-value">#{hexColor}</p>
     </article>
   );
 };
