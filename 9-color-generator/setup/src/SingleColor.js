04943ebff8315310e66b49ab9eda9b88870757c8
Kenneth <KennethZhang374@gmail.com>
2021-02-03 13:51:38 -0800

    complete

--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from "react";
 
 const SingleColor = ({ rgb, index, weight, hexColor }) => {
-  const [alter, setAlert] = useState(false);
+  const [alert, setAlert] = useState(false);
   const bcg = rgb.join(","); //turn array to string
+  let hexValue = `#${hexColor}`;
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
   return (
-    <article className={`color`} style={{ backgroundColor: `rgb(${bcg})` }}>
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
       <p className="percent-value">{weight}%</p>
-      <p className="color-value">#{hexColor}</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert"> copied to clipboard</p>}
     </article>
   );
 };
