e93b1fd27f6074daa0bebdc3df093da31f938b4e
Kenneth <KennethZhang374@gmail.com>
2021-02-03 00:02:00 -0800

    set up form and input

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,32 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form onSubmit={handleSubmit} className="lorem-form">
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={e => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          {" "}
+          generate
+        </button>
+      </form>
+    </section>
+  );
 }
 
 export default App;
