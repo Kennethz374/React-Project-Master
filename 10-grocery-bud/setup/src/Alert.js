60b44cfde807bca660851b9b2950e8c2a0f2f65c
Kenneth <KennethZhang374@gmail.com>
2021-02-03 14:59:51 -0800

    done clear items and remove item

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
 
 //showAlert() did not pass in any parameters, return to default value
-const Alert = ({ type, msg, removeAlert }) => {
+const Alert = ({ type, msg, removeAlert, list }) => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       removeAlert();
     }, 2000);
     return () => clearTimeout(timeout);
-  }, []);
+  }, [list]);
   return <p className={`alert alert-${type}`}>{msg}</p>;
 };
 
--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -20,8 +20,6 @@ function App() {
     } else if (name && isEditing) {
       //deal with edit
     } else {
-      //show alert
-      //show list
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
       setName("");
@@ -33,10 +31,23 @@ function App() {
     setAlert({ show, type, msg });
   };
 
+  const clearList = () => {
+    showAlert(true, "danger", "empty the list");
+    setList([]);
+  };
+
+  const removeItem = id => {
+    showAlert(true, "danger", "item has been removed");
+    setList(list.filter(item => item.id !== id));
+  };
+  const editItem = id => {
+    setIsEditing(true);
+  };
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input
@@ -55,8 +66,10 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} />
-          <button className="clear-btn">clear items</button>
+          <List items={list} removeItem={removeItem} />
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
         </div>
       )}
     </section>
