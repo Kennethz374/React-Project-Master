3fba2f9083c49f5c98356edbc19140f6f70850a4
Kenneth <KennethZhang374@gmail.com>
2021-02-03 22:11:28 -0800

    keep all data in one place

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,42 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  return (
+    <>
+      <nav className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="coding addict" />
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container show-container">
+          <ul className="links">
+            {links.map(link => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map(item => {
+            return (
+              <li key={item.id}>
+                <a href={item.url}>{item.icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
