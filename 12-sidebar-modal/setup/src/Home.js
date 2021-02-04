dacb18072123c6fa4122b75555f5fedefa4b7fcc
Kenneth <KennethZhang374@gmail.com>
2021-02-04 13:32:24 -0800

    wrapping whole app in useContext

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,7 +1,10 @@
 import React, { useContext } from "react";
 import { FaBars } from "react-icons/fa";
-
+import { AppContext, useGlobalContext } from "./context";
+import App from "./App";
 const Home = () => {
+  const data = useGlobalContext();
+  console.log(data);
   return (
     <main>
       <button className="sidebar-toggle">
