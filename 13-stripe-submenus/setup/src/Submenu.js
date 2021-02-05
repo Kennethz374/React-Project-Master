5eb409f5231add843fb8eee285ae77fc3d42ed54
Kenneth <KennethZhang374@gmail.com>
2021-02-05 00:01:08 -0800

    submenu onMouseOver

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -2,9 +2,20 @@ import React, { useState, useRef, useEffect } from "react";
 import { useGlobalContext } from "./context";
 
 const Submenu = () => {
-  const { isSubmenuOpen } = useGlobalContext();
+  const { isSubmenuOpen, location } = useGlobalContext();
+  const container = useRef(null);
+  useEffect(() => {
+    const submenu = container.current; //gets the dom node
+    const { center, bottom } = location; //get the coordinates
+    submenu.style.left = `${center}px`; //change css
+    submenu.style.top = `${bottom}px`; //change css
+  }, [location]); //everytime location change, it trigger re-render submenu dom
+
   return (
-    <aside className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}>
+    <aside
+      ref={container}
+      className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}
+    >
       submenu
     </aside>
   );
