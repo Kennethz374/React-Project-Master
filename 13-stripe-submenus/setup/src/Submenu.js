7233f099d6055bca0388d7fcab2f673c6cb6a10b
Kenneth <KennethZhang374@gmail.com>
2021-02-05 00:32:45 -0800

    submenu render ok

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -2,7 +2,11 @@ import React, { useState, useRef, useEffect } from "react";
 import { useGlobalContext } from "./context";
 
 const Submenu = () => {
-  const { isSubmenuOpen, location } = useGlobalContext();
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links }
+  } = useGlobalContext();
   const container = useRef(null);
   useEffect(() => {
     const submenu = container.current; //gets the dom node
@@ -16,7 +20,18 @@ const Submenu = () => {
       ref={container}
       className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}
     >
-      submenu
+      <h4>{page}</h4>
+      <div className={`submenu-center col-2`}>
+        {links.map((link, index) => {
+          const { label, icon, url } = link;
+          return (
+            <a href={url} key={index}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
     </aside>
   );
 };
