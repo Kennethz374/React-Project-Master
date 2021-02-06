eda4d44cb8ec25fb06bcdd43b75ca3f8ee5f9dbc
Kenneth <KennethZhang374@gmail.com>
2021-02-05 16:14:58 -0800

    expression bug, fixed

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -1,4 +1,15 @@
 const reducer = (state, action) => {
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+  if (action.type === "REMOVE") {
+    const { cart } = state;
+    const newCart = cart.filter(item => item.id !== action.payload);
+    return {
+      ...state,
+      cart: newCart
+    };
+  }
   return state;
 };
 
