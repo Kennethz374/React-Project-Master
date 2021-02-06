2ea6728e24a0d873ea5d2d388a673835d14b1853
Kenneth <KennethZhang374@gmail.com>
2021-02-05 16:34:17 -0800

    complete increase and decrease functions

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -1,3 +1,5 @@
+import CartItem from "./CartItem";
+
 const reducer = (state, action) => {
   if (action.type === "CLEAR_CART") {
     return { ...state, cart: [] };
@@ -9,6 +11,29 @@ const reducer = (state, action) => {
       ...state,
       cart: newCart
     };
+  }
+
+  if (action.type === "INCREASE") {
+    let tempCart = state.cart.map(item => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+    return { ...state, cart: tempCart };
+  }
+
+  if (action.type === "DECREASE") {
+    let tempCart = state.cart
+      .map(item => {
+        if (item.id === action.payload) {
+          return { ...item, amount: item.amount - 1 };
+        }
+        return item;
+      })
+      .filter(cartItem => cartItem.amount !== 0);
+
+    return { ...state, cart: tempCart };
   }
   return state;
 };
