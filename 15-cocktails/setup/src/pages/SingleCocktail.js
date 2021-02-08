b42b102a1513936ebb0eb2ec5164dfe1e548eb3f
Kenneth <KennethZhang374@gmail.com>
2021-02-08 13:25:35 -0800

    complete the search form

--- 15-cocktails/setup/src/pages/SingleCocktail.js
+++ 15-cocktails/setup/src/pages/SingleCocktail.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import Loading from '../components/Loading'
-import { useParams, Link } from 'react-router-dom'
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
+import React from "react";
+import Loading from "../components/Loading";
+import { useParams, Link } from "react-router-dom";
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const SingleCocktail = () => {
+  // const fetchDrink = async () => {
+  //   const response = await fetch(`${url}${useParams}`);
+  //   const drink = await response.json();
+  //   const {strDrink}
+  // };
   return (
     <div>
       <h2>single cocktail page </h2>
     </div>
-  )
-}
+  );
+};
 
-export default SingleCocktail
+export default SingleCocktail;
