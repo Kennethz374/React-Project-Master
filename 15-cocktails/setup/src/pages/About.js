882bc3f6f6396b1a020cd735d38f63dc275f749c
Kenneth <KennethZhang374@gmail.com>
2021-02-08 11:24:25 -0800

    start setting up simple pages.About page completed

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+        recusandae numquam consequatur nihil, odio, aut, atque nesciunt
+        accusantium nemo laborum perferendis maxime fuga ut ipsam nulla
+        excepturi labore sint voluptas culpa dicta ipsum! Nulla nobis voluptates
+        ut aperiam eveniet aliquam ducimus, incidunt dolores officia
+        exercitationem libero minima atque architecto odio!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
